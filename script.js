e04a4e9fea781b38966eb2fbe80bff754b30e4e0
Gabriel Silva Araujo <86019173+MystGabriel@users.noreply.github.com>
2021-09-01 13:35:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+$('.portfolio-item').isotope({
+    itemSelector: '.item',
+    layoutMode: 'packery',
+    percentPosition: true,
+    mansory: {
+        columnWidth: '.item'
+    }
+});
+
+$('.portfolio-menu ul li').click(function(){
+    $('.portfolio-menu ul li').removeClass('active');
+    $(this).addClass('active');
+
+    var selector = $(this).attr('data-filter');
+
+    $('.portfolio-item').isotope({
+        filter: selector
+    });
+
+    return false;
+});
+
+$(document).ready(function(){
+    $('.portfolio-menu ul li').click(function(){
+        const value = $(this).attr('data-filter');
+
+        if(value == '*'){
+            $('.item').show('1000');
+        }else{
+            $('.item').not('.'+value).hide('1000');
+            $('.item').filter('.'+value).show('1000');
+        }
+    });
+});
